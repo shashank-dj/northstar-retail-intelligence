b91255aa6d03350850ab73fdbf5b05d7aba3465d
SHASHANK <shashankdj622@gmail.com>
2026-01-18 22:48:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
+import Analyze from "./pages/Analyze"
 
-function App() {
-  return <Home />
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/analyze" element={<Analyze />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App
