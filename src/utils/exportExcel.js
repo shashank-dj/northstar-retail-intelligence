fea439fccaf0b64819a0558d2058492d17fb7ca0
SHASHANK <shashankdj622@gmail.com>
2026-02-01 20:01:45 +0100

    Update exportExcel.js

--- src/utils/exportExcel.js
+++ src/utils/exportExcel.js
@@ -1,1 +1,23 @@
 
+import * as XLSX from "xlsx"
+
+export function exportFinancialExcel(area, revenue, financials) {
+  const data = [
+    ["Metric", "Value"],
+    ["Location", `${area.area}, ${area.city}`],
+    ["Monthly Revenue", revenue],
+    ["EBITDA", financials.ebitda],
+    ["Payback (months)", financials.paybackMonths],
+  ]
+
+  const worksheet = XLSX.utils.aoa_to_sheet(data)
+  const workbook = XLSX.utils.book_new()
+
+  XLSX.utils.book_append_sheet(
+    workbook,
+    worksheet,
+    "Financial Model"
+  )
+
+  XLSX.writeFile(workbook, "northstar-financial-model.xlsx")
+}
