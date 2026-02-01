3894f6d813720b1ca5c0fc390e88b87bf24fe1de
SHASHANK <shashankdj622@gmail.com>
2026-02-01 10:52:34 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Analyze from "./pages/Analyze"
 import Recommend from "./pages/Recommend"
+import Financials from "./pages/Financials"
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/analyze" element={<Analyze />} />
         <Route path="/recommend" element={<Recommend />} />
+        <Route path="/financials" element={<Financials />} />
       </Routes>
     </BrowserRouter>
   )
